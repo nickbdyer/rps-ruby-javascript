0ea1db0f6c5021540481bc4f18e967555b06bef8
Nick Dyer <nbdyer@gmail.com>
2014-11-25 15:46:10 +0000

    Refactor pairs hash into game object

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -11,17 +11,18 @@ function Game(player1, player2) {
   this.player2 = player2;
 };
 
+Game.prototype.pairs = {
+  rock: 'scissors', 
+  paper: 'rock',
+  scissors: 'paper'
+  }
+
+
 Game.prototype.winner = function(player1, player2) {
-var pairs = { 'rock' : 'scissors', 'paper' : 'rock', 'scissors' : 'paper'}
-  if (this.samePick()) { 
-    return null 
-  }
-  else if (pairs[this.player1.pick] === this.player2.pick) { 
-    return this.player1 
-  }
-  else { 
-    return this.player2 }
-  }
+  if (this.samePick()) { return null }
+  else if (this.pairs[this.player1.pick] === this.player2.pick) { return this.player1 }
+  else { return this.player2 } 
+}
 
 Game.prototype.samePick = function() {
   return this.player1.pick === this.player2.pick;
