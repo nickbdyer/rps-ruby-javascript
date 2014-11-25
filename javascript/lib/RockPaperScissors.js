1f611aee6669a373aa77552834ecca2a753e889e
Nick Dyer <nbdyer@gmail.com>
2014-11-25 14:27:33 +0000

    Game logic complete

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -6,17 +6,18 @@ Player.prototype.picks = function(pick) {
   this.pick = pick;
 };
 
-
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
 };
 
 Game.prototype.winner = function(player1, player2) {
-  if (this.draw) return null;
-  else if ()
+  var pairs = { 'rock' : 'scissors', 'paper' : 'rock', 'scissors' : 'paper'}
+  if (this.player1.pick === this.player2.pick) { return null }
+  else if (pairs[this.player1.pick] === this.player2.pick) { return this.player1 }
+    else { return this.player2 }
 }
 
-Game.prototype.draw = function(player1, player2) {
-  player1.pick === player2.pick;
-}
+// Game.prototype.draw = function(player1, player2) {
+//   if (this.draw) { return null }
+// }
