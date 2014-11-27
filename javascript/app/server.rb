8bf7eafcae32c2ad474f4baf0b93c105fa4f7ca4
Nick Dyer <nbdyer@gmail.com>
2014-11-27 14:20:18 +0000

    Convert to sinatra app

---
+++ javascript/app/server.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+set :public_folder, Proc.new { File.join(root, "..", "public") }
+set :views, Proc.new { File.join(root, "views") }
+
+
+get '/' do
+  erb :index
+end
+
---
+++ javascript/public/javascripts/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  var remove = function() {
+    $(this).remove();
+  };
+
+  var render = function(game){
+    var html = $('#list-item-template').html();
+    var data = {message: game.victoryMessage()};
+    return Mustache.render(html, data);
+  };
+
+  var ben = new Player('Ben');
+  var nick = new Player('Nick');
+  var tron = new Computer('Tron')
+  var game = new Game(ben, tron);
+
+
+  $('.choices span').on('click', function() {
+    ben.picks($(this).data('pick'));
+    tron.picks();
+    $(render(game)).prependTo('#results').slideDown();
+    $('li:nth-child(5)').fadeOut(3000,remove);
+  });
+
+  $('.choices').mouseenter(function(e) {
+    $(this).children('span').fadeIn(200);
+  }).mouseleave(function(e) {
+    $(this).children('span').fadeOut(200);
+  });
+
+});
